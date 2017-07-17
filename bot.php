a059ced8d2c86ae5aabef66043b0158c89328486
bankincity <bank.t.2002@gmail.com>
2017-07-17 18:46:48 +0700

    Update bot.php
    Use namespace

--- bot.php
+++ bot.php
@@ -1,5 +1,9 @@
 <?php
 
+use LINE;
+
+$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient('<channel access token>');
+
 $access_token = 'F2bejKi+Zd2+2VIs9YrRAt4sctvtJv6MMDlPN6kTh9eo1Xoh/Lq8021aEtL0yDk7yh5HYkaQEbvM52DJR7yNt162DM5dyg0qPkdZneUrAX1mrgkxaP3KDee0CnhdZARnowHeuggOVRJI7L98Knm7PwdB04t89/1O/w1cDnyilFU=';
 
 // Get POST body content
