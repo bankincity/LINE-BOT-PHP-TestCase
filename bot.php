dda9ff35818e3e640628ac6ab7f5bd85ae40fd6d
bankincity <bank.t.2002@gmail.com>
2017-08-08 22:05:08 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -2,7 +2,7 @@
 
 use LINE;
 
-$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient;
+$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient('F2bejKi+Zd2+2VIs9YrRAt4sctvtJv6MMDlPN6kTh9eo1Xoh/Lq8021aEtL0yDk7yh5HYkaQEbvM52DJR7yNt162DM5dyg0qPkdZneUrAX1mrgkxaP3KDee0CnhdZARnowHeuggOVRJI7L98Knm7PwdB04t89/1O/w1cDnyilFU=');
 $access_token = 'F2bejKi+Zd2+2VIs9YrRAt4sctvtJv6MMDlPN6kTh9eo1Xoh/Lq8021aEtL0yDk7yh5HYkaQEbvM52DJR7yNt162DM5dyg0qPkdZneUrAX1mrgkxaP3KDee0CnhdZARnowHeuggOVRJI7L98Knm7PwdB04t89/1O/w1cDnyilFU=';
 
 // Get POST body content
