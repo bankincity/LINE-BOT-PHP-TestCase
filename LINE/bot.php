c89b729d85060acbc1b860c288bffd422e41b6f7
bankincity <bank.t.2002@gmail.com>
2017-08-10 22:07:39 +0700

    Create bot.php

---
+++ LINE/bot.php
@@ -0,0 +1,58 @@
+
+
+<?php
+echo "Stup1";
+
+namespace LINE
+
+echo "Stup2";
+$httpClient = new LINE\LINEBot\HTTPClient\CurlHTTPClient('F2bejKi+Zd2+2VIs9YrRAt4sctvtJv6MMDlPN6kTh9eo1Xoh/Lq8021aEtL0yDk7yh5HYkaQEbvM52DJR7yNt162DM5dyg0qPkdZneUrAX1mrgkxaP3KDee0CnhdZARnowHeuggOVRJI7L98Knm7PwdB04t89/1O/w1cDnyilFU=');
+$access_token = 'F2bejKi+Zd2+2VIs9YrRAt4sctvtJv6MMDlPN6kTh9eo1Xoh/Lq8021aEtL0yDk7yh5HYkaQEbvM52DJR7yNt162DM5dyg0qPkdZneUrAX1mrgkxaP3KDee0CnhdZARnowHeuggOVRJI7L98Knm7PwdB04t89/1O/w1cDnyilFU=';
+echo "Stup3";
+// Get POST body content
+$content = file_get_contents('php://input');
+// Parse JSON
+$events = json_decode($content, true);
+// Validate parsed JSON data
+if (!is_null($events['events'])) {
+	// Loop through each event
+	foreach ($events['events'] as $event) {
+		
+		// Reply only when message sent is in 'text' format
+		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
+			// Get text sent
+			$text = $event['message']['text'];
+			// Get replyToken
+			$replyToken = $event['replyToken'];
+
+			// Build message to reply back
+			$messages = [
+				'type' => 'text',
+				'text' => $text.'กวน'
+			];
+
+			// Make a POST Request to Messaging API to reply to sender
+			$url = 'https://api.line.me/v2/bot/message/reply';
+			$data = [
+				'replyToken' => $replyToken,
+				'messages' => [$messages],
+			];
+			$post = json_encode($data);
+			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+
+			$ch = curl_init($url);
+			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+			curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+			$result = curl_exec($ch);
+			curl_close($ch);
+
+			echo $result . "\r\n";
+		}
+	}
+}
+echo "OK";
+
+?>
